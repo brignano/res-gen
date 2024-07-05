7d88165766b6f22f4fa2a2f48c5d33d46a088ef9
Anthony Brignano <abrignano@comcast.net>
2024-07-05 19:46:04 -0400

    add `RESUME_URL` to `publicRuntimeConfig`

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    publicRuntimeConfig: {
+        RESUME_URL: "https://gist.github.com/brignano/99d784fa160c1d40f29c64a9e36b50a3"
+    }
+};
 
 export default nextConfig;
