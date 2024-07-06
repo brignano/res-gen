311fd3b6f311d7570ec0d77eded286b0eb2282f1
Anthony Brignano <abrignano@comcast.net>
2024-07-06 11:11:51 -0400

    make `GIST_ID` an environment variable

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     publicRuntimeConfig: {
-        gistId: "99d784fa160c1d40f29c64a9e36b50a3"
+        gistId: process.env.GIST_ID
     }
 };
 
