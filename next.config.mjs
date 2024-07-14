2a04bb32c271dbaf32e0f37ba98e66e733ea4214
anthony <abrignano@comcast.net>
2024-07-14 15:26:06 +0000

    remove nextjs configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    reactStrictMode: true,
-    basePath: "/res-gen",
     publicRuntimeConfig: {
         jsonResumeUrl: process.env.JSON_RESUME_URL
     }
