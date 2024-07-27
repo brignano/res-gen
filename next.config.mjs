e9f2c33936e57c3f128de62da266121112ae7ec3
anthony <abrignano@comcast.net>
2024-07-27 05:17:48 +0000

    prefix `JSON_RESUME_URL` with `NEXT_PUBLIC_`

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     publicRuntimeConfig: {
-        jsonResumeUrl: process.env.JSON_RESUME_URL
+        jsonResumeUrl: process.env.NEXT_PUBLIC_JSON_RESUME_URL
     },
     serverRuntimeConfig: {
         googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
