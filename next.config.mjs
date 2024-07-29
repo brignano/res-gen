858600d752120c80f716d623034720b8d056fbbd
anthony <abrignano@comcast.net>
2024-07-29 23:20:10 +0000

    add `skipTrailingSlashRedirect` to `next.config.mjs`

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: { unoptimized: true }, basePath: "/resume", output: "export",
+    skipTrailingSlashRedirect: true,
     publicRuntimeConfig: {
         jsonResumeUrl: process.env.NEXT_PUBLIC_JSON_RESUME_URL
     },
