453a0f996905d722eaa5bf2f0c065a7cff864919
anthony <abrignano@comcast.net>
2024-07-29 23:13:45 +0000

    write variables to .env file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: { unoptimized: true }, basePath: "/resume", output: "export",
     publicRuntimeConfig: {
         jsonResumeUrl: process.env.NEXT_PUBLIC_JSON_RESUME_URL
     },
